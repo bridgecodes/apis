064815f7301c542f2e66cb2a459b1f34cd4f8398
bridgecodes <support@farmy.in>
2015-08-07 00:31:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $app->get('/parent-cat','getParentCat');
 $app->get('/subcat/:id',	'getSubCat');
 $app->get('/cat-item-list/:id',	'getItemCat');
 $app->get('/get-item-details/:id',	'getItemDetails');
-$app->get('/test/:id',	'test');
+
 
 $app->run();
 function getParentCat() {
@@ -111,36 +111,6 @@ $sql3="SET character_set_results=utf8";
 		$db = null;
 		 $arr[] = array('body'=>$Item,'image'=>$items);
 		echo '{"item": ' . json_encode($arr) . '}';
-	} catch(PDOException $e) {
-		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
-	}
-}
-function test($id) {
-	$sql = "SELECT DISTINCT (os_t_item.pk_i_id) , os_t_item_description.s_title, os_t_item.dt_pub_date, os_t_item.i_price, os_t_item.fk_c_currency_code, os_t_category_description.s_name,os_t_item_resource.pk_i_id AS image_name,os_t_item_resource.s_extension as image_format,os_t_item_resource.s_path as img_url, os_t_item_location.s_region as state,os_t_item_location.s_city as city
-FROM os_t_item 
-LEFT OUTER JOIN os_t_category_description ON os_t_item.fk_i_category_id=os_t_category_description.fk_i_category_id
-
-inner JOIN os_t_item_description ON os_t_item_description.fk_i_item_id = os_t_item.pk_i_id
-left JOIN os_t_item_resource ON os_t_item.pk_i_id = os_t_item_resource.fk_i_item_id
-left join os_t_item_location on os_t_item_location.fk_i_item_id=os_t_item.pk_i_id
-
-where os_t_category_description.fk_c_locale_code='en_US' AND 
-os_t_item.fk_i_category_id=:id group by os_t_item.pk_i_id order by os_t_item.dt_pub_date  ";
-$sql2="SET character_set_results=utf8";
-
-	try {
-		$db = getConnection();
-		$stmt2 = $db->prepare($sql2); 
-		$stmt = $db->prepare($sql);  
-		
-		$stmt->bindParam("id", $id);
-		$stmt2->execute();
-		$stmt->execute();
-	
-		$items = $stmt->fetchAll(PDO::FETCH_OBJ);	
-		$db = null;
-			
-		echo '{"items": ' . json_encode($items) .' }';
 	} catch(PDOException $e) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}'; 
 	}
